eade183664c3bc7ab7cd09cf58df7ced1e85130c
WederPacheco <wederpacheco.s@gmail.com>
2017-11-07 14:36:44 -0200

    init: project setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// @flow
+
+import * as React from 'react';
+import { Route } from 'react-router-dom';
+
+type Props = {};
+type State = {};
+
+class App extends React.Component<Props, State> {
+  render() {
+    return (
+      <div className="App">
+        Olá
+      </div>
+    );
+  }
+}
+
+export default App;
