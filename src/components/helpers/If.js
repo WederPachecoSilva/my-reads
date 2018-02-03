47470a7f1e6311532acb58631bc0f65248dcc0b0
wpsilva <wederpacheco.s@gmail.com>
2018-02-03 12:34:02 -0200

    bug fix: truthy check bug fixed on If component

--- src/components/helpers/If.js
+++ src/components/helpers/If.js
@@ -2,8 +2,8 @@ import * as React from 'react';
 
 const If = props => {
     const { condition, children } = props;
-    if (condition === true) {
-        return <div>{children}</div>;
+    if (condition) {
+        return <React.Fragment>{children}</React.Fragment>;
     } else {
         return null;
     }
